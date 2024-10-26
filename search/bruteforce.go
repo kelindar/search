f7436abdb6dc04835f45843bc0b44d16b5d8f1d8
Roman <roman.atachiants@gmail.com>
2024-10-27 00:52:51 +0400

    heap based

--- search/bruteforce.go
+++ search/bruteforce.go
@@ -3,6 +3,7 @@ package search
 import (
 	"cmp"
 	"sort"
+	"sync"
 )
 
 type entry[T cmp.Ordered] struct {
@@ -16,14 +17,22 @@ type Result[T cmp.Ordered] struct {
 }
 
 type Bag[T cmp.Ordered] struct {
-	arr []entry[T]
-	dim int
+	arr  []entry[T]
+	dim  int
+	pool sync.Pool
 }
 
 func NewBag[T cmp.Ordered](dim int) *Bag[T] {
 	return &Bag[T]{
 		arr: make([]entry[T], 0),
 		dim: dim,
+		/*pool: sync.Pool{
+			New: func() any {
+				top := &topk[T]{}
+				heap.Init(top)
+				return top
+			},
+		},*/
 	}
 }
 
@@ -35,21 +44,164 @@ func (b *Bag[T]) Add(vx Vector, data T) {
 }
 
 // Search implements a brute-force search algorithm to find the k-nearest neighbors
-func (b *Bag[T]) Search(query Vector, k int) []Result[T] {
+func (b *Bag[T]) Search(query Vector, k int) []Result[T] { //iter.Seq[Result[T]] {
+	if k <= 0 {
+		return nil
+	}
 
-	// Compute the distance to each vector
-	results := make([]Result[T], len(b.arr))
-	for i, v := range b.arr {
-		results[i] = Result[T]{
-			Relevance: Cosine(v.Vector, query),
+	/*h := b.pool.Get().(*topk[T])
+	h.Reset()
+	defer b.pool.Put(h)*/
+
+	//h := &minheap[T]{}
+	var h minheap[T]
+
+	for _, v := range b.arr {
+		relevance := Cosine(v.Vector, query)
+		result := Result[T]{
 			entry:     v,
+			Relevance: relevance,
+		}
+
+		if h.Len() < k {
+			//heap.Push(h, result)
+			h.Push(result)
+		} else if relevance > h[0].Relevance {
+			//heap.Pop(h)
+			//heap.Push(h, result)
+			h.Pop()
+			h.Push(result)
 		}
 	}
 
-	// Sort the distances
-	sort.Slice(results, func(i, j int) bool {
-		return results[i].Relevance > results[j].Relevance
+	sort.Slice(h, func(i, j int) bool {
+		return h[i].Relevance > h[j].Relevance
 	})
 
-	return results[:k]
+	return h[:k]
+
+	// Extract results from the heap and sort them in descending order of relevance
+	/*topResults := make([]Result[T], h.Len())
+	for i := len(topResults) - 1; i >= 0; i-- {
+		//topResults[i] = heap.Pop(h).(Result[T])
+		topResults[i] = h.Pop()
+	}
+
+	return topResults*/
+	/*return func(yield func(Result[T]) bool) {
+		for h.Len() > 0 {
+			if !yield(heap.Pop(h).(Result[T])) {
+				return
+			}
+		}
+	}*/
+}
+
+// --------------------------------- Heap ---------------------------------
+/*
+// topk is a min-heap that keeps the top k results with the highest relevance.
+type topk[T cmp.Ordered] []Result[T]
+
+// Implement heap.Interface for topKHeap
+func (h topk[T]) Len() int           { return len(h) }
+func (h topk[T]) Less(i, j int) bool { return h[i].Relevance < h[j].Relevance } // Min-heap
+func (h topk[T]) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+// Push adds an element to the heap.
+func (h *topk[T]) Push(x interface{}) {
+	*h = append(*h, x.(Result[T]))
+}
+
+// Pop removes and returns the smallest element from the heap.
+func (h *topk[T]) Pop() interface{} {
+	old := *h
+	n := len(old)
+	item := old[n-1]
+	*h = old[0 : n-1]
+	return item
+}
+
+func (h *topk[T]) Reset() {
+	*h = (*h)[:0]
+}*/
+
+// minheap is a min-heap of top values, ordered by count.
+type minheap[T cmp.Ordered] []Result[T]
+
+// Reset resets the minheap to an empty state.
+func (h *minheap[T]) Reset() {
+	*h = (*h)[:0]
+}
+
+// Len, Less, Swap implement the sort.Interface.
+func (h *minheap[T]) Len() int           { return len(*h) }
+func (h *minheap[T]) Less(i, j int) bool { return (*h)[i].Relevance < (*h)[j].Relevance }
+func (h *minheap[T]) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+
+// Push adds a new element to the heap.
+func (h *minheap[T]) Push(x Result[T]) {
+	*h = append(*h, x)
+	h.up(h.Len() - 1)
+}
+
+// Pop returns the minimum element from the heap.
+func (h *minheap[T]) Pop() Result[T] {
+	n := h.Len() - 1
+	h.Swap(0, n)
+	h.down(0, n)
+
+	// Pop the last element
+	x := (*h)[n]
+	*h = (*h)[:n]
+	return x
+}
+
+// Update updates the count of the element at index i.
+func (h minheap[T]) Update(i int, relevance float64) {
+	h[i].Relevance = relevance
+	if !h.down(i, len(h)) {
+		h.up(i)
+	}
+}
+
+// Clone clones the minheap into dst.
+func (h minheap[T]) Clone(dst *minheap[T]) {
+	for _, e := range h {
+		if e.Relevance > 0 {
+			(*dst) = append(*dst, e)
+		}
+	}
+}
+
+func (h minheap[T]) up(j int) {
+	for {
+		i := (j - 1) / 2 // parent
+		if i == j || !(h[j].Relevance < h[i].Relevance) {
+			break
+		}
+
+		h[i], h[j] = h[j], h[i]
+		j = i
+	}
+}
+
+func (h minheap[T]) down(at, n int) bool {
+	i := at
+	for {
+		j1 := 2*i + 1
+		if j1 >= n || j1 < 0 { // j1 < 0 after int overflow
+			break
+		}
+		j := j1 // left child
+		if j2 := j1 + 1; j2 < n && (h[j2].Relevance < h[j1].Relevance) {
+			j = j2 // = 2*i + 2  // right child
+		}
+		if h[i].Relevance < h[j].Relevance {
+			break
+		}
+
+		h[i], h[j] = h[j], h[i]
+		i = j
+	}
+	return i > at
 }
--- search/bruteforce_test.go
+++ search/bruteforce_test.go
@@ -10,7 +10,7 @@ import (
 
 /*
 cpu: 13th Gen Intel(R) Core(TM) i7-13700K
-BenchmarkBruteForce-24    	     889	   1311187 ns/op	  147752 B/op	       5 allocs/op
+BenchmarkBruteForce-24    	    4209	    295092 ns/op	     720 B/op	       4 allocs/op
 */
 func BenchmarkBruteForce(b *testing.B) {
 	data, err := loadDataset()
@@ -24,7 +24,9 @@ func BenchmarkBruteForce(b *testing.B) {
 	b.ReportAllocs()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_ = bag.Search(data[i%1000].Vector, 5)
+		for v := range bag.Search(data[i%1000].Vector, 5) {
+			_ = v
+		}
 	}
 }
 
@@ -50,92 +52,3 @@ func loadDataset() ([]record, error) {
 
 	return data, nil
 }
-
-/*func BenchmarkBruteForce(b *testing.B) {
-	entries, err := loadSICK()
-	if err != nil {
-		b.Fatal(err)
-	}
-
-	llm := loadModel()
-	defer llm.Close()
-
-	var data []entry
-
-	uniq := make(map[string]struct{})
-
-	for _, entry := range entries {
-		text1 := entry.Pair[0]
-		if _, ok := uniq[text1]; !ok {
-			uniq[entry.Label] = struct{}{}
-			entry.Vector, _ = llm.EmbedText(entry.Pair[0])
-			data = append(data, entry)
-		}
-
-		text2 := entry.Pair[1]
-		if _, ok := uniq[text2]; !ok {
-			uniq[entry.Label] = struct{}{}
-			entry.Vector, _ = llm.EmbedText(entry.Pair[1])
-			data = append(data, entry)
-		}
-	}
-
-	f, _ := os.OpenFile("dataset.gob", os.O_CREATE|os.O_RDWR, 0644)
-	w := gob.NewEncoder(f)
-	w.Encode(data)
-	f.Close()
-
-}
-
-func loadModel() *llm.Model {
-	model := "../dist/MiniLM-L6-v2.Q8_0.gguf"
-	fmt.Printf("Loading model: %s\n", model)
-
-	mod, _ := filepath.Abs(model)
-	ctx, err := llm.New(mod, 0)
-	if err != nil {
-		panic(err)
-	}
-
-	return ctx
-}
-
-
-// loadSICK parses the SICK CSV dataset and returns sentence pairs with their relatedness scores
-func loadSICK() ([]entry, error) {
-	file, err := os.Open("../eval/dataset.txt")
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	reader := csv.NewReader(file)
-	reader.Comma = '\t'    // Tab-separated file
-	_, err = reader.Read() // Skip header line
-	if err != nil {
-		return nil, err
-	}
-
-	out := make([]entry, 0, 4600)
-	for {
-		record, err := reader.Read()
-		if err != nil {
-			break
-		}
-
-		sentenceA := record[1]
-		sentenceB := record[2]
-		relatednessScore, err := strconv.ParseFloat(record[3], 64)
-		if err != nil {
-			return nil, err
-		}
-
-		out = append(out, entry{
-			Pair:  [2]string{sentenceA, sentenceB},
-			Rank:  relatednessScore,
-			Label: record[4],
-		})
-	}
-
-	return out, nil
-}*/
