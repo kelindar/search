8e51c81d67481934dda2b1523b755c4c3337bfc6
Roman <roman.atachiants@gmail.com>
2024-10-27 16:51:40 +0400

    add stub

---
+++ internal/cosine/simd/cosine_stub.go
@@ -0,0 +1,8 @@
+//go:build !noasm && !(amd64 || (darwin && arm64) || (!darwin && arm64))
+
+package simd
+
+import "unsafe"
+
+// stub
+func f32_cosine_distance(x unsafe.Pointer, y unsafe.Pointer, result unsafe.Pointer, size uint64)
